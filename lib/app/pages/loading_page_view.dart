83dc422b4fbb0c109b35bb1285716787d8ca40c0
tokujiTO <tiagomassuda123@gmail.com>
2025-08-19 14:39:33 -0300

    feat: add logos and loading page

--- lib/app/pages/loading_page_view.dart
+++ lib/app/pages/loading_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poliedroimagesgenerator/app/utils/app_colors.dart';
 
 class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
@@ -8,12 +9,34 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+  bool _isAnimating = true;
+  bool _forward = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      body: Center(child: CircularProgressIndicator(color: Colors.red)),
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: TweenAnimationBuilder<double>(
+          tween: Tween(begin: _forward ? 1.0 : 1.1, end: _forward ? 1.1 : 1.0),
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeInOut,
+          builder: (context, scale, _) {
+            return Transform.scale(
+              scale: scale,
+              child: Image.asset('lib/app/assets/verticalColored.png'),
+            );
+          },
+          onEnd: () {
+            if (mounted && _isAnimating) {
+              setState(() {
+                _forward = !_forward;
+              });
+            }
+          },
+        ),
+      ),
     );
   }
 }
