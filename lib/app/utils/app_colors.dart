0177f9ee8f6948922050d8b51d4c9740a33ba3eb
tokujiTO <tiagomassuda123@gmail.com>
2025-08-19 13:34:22 -0300

    feat: add colortheme, main root, and general pages

---
+++ lib/app/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFF4F4F4);
+  static const red = Color(0xFFF2275D);
+  static const blue = Color(0xFF21BFBF);
+  static const yellow = Color(0xFFFBA41F);
+}
