42aa86d73a4a10a19f2ffff07a6ac6c761f795dc
Giovanna Lara <ghlar4sr@gmail.com>
2025-11-23 00:59:23 -0300

    feat: fix visual web

--- lib/app/pages/signup_page.dart
+++ lib/app/pages/signup_page.dart
@@ -27,6 +27,112 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final isWeb = size.width > 800; // define web layout
+
+    Widget fields = Column(
+      children: [
+        // Nome
+        SizedBox(
+          width: isWeb ? 500 : size.width * 0.8,
+          child: CustomTextField(
+            focusBorder: AppColors.yellow,
+            controller: controller.nameController,
+            labelText: 'Nome',
+            onChanged: (value) => controller.setName(value),
+            suffixIcon: Icon(Icons.person, color: AppColors.white),
+          ),
+        ),
+        SizedBox(height: 16),
+
+        // Email
+        SizedBox(
+          width: isWeb ? 500 : size.width * 0.8,
+          child: CustomTextField(
+            focusBorder: AppColors.yellow,
+            suffixIcon: Icon(Icons.mail, color: AppColors.white),
+            labelText: 'E-mail',
+            onChanged: (value) => controller.setEmail(value),
+            keyboardType: TextInputType.emailAddress,
+            controller: controller.emailController,
+          ),
+        ),
+        SizedBox(height: 16),
+
+        // Senha
+        SizedBox(
+          width: isWeb ? 500 : size.width * 0.8,
+          child: CustomTextField(
+            focusBorder: AppColors.yellow,
+            controller: controller.passwordController,
+            labelText: 'Senha',
+            onChanged: (value) => controller.setPassword(value),
+            suffixIcon: controller.isPasswordVisible
+                ? Icon(Icons.visibility, color: AppColors.white)
+                : Icon(Icons.visibility_off, color: AppColors.white),
+            isPassword: !controller.isPasswordVisible,
+            onSuffixIconTap: () {
+              setState(() {
+                controller.togglePasswordVisibility();
+              });
+            },
+          ),
+        ),
+        SizedBox(height: 16),
+
+        // Confirmar senha
+        SizedBox(
+          width: isWeb ? 500 : size.width * 0.8,
+          child: CustomTextField(
+            focusBorder: AppColors.yellow,
+            controller: controller.confirmPasswordController,
+            labelText: 'Confirmar Senha',
+            suffixIcon: controller.isConfirmPasswordVisible
+                ? Icon(Icons.visibility, color: AppColors.white)
+                : Icon(Icons.visibility_off, color: AppColors.white),
+            isPassword: !controller.isConfirmPasswordVisible,
+            onSuffixIconTap: () {
+              setState(() {
+                controller.toggleConfirmPasswordVisibility();
+              });
+            },
+          ),
+        ),
+        SizedBox(height: 24),
+      ],
+    );
+
+    Widget signupButton = Hero(
+      tag: 'signupBtn',
+      child: SizedBox(
+        width: isWeb ? 500 : size.width * 0.8,
+        height: size.height * 0.062,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            elevation: WidgetStateProperty.all(2),
+            backgroundColor: WidgetStateProperty.all(Colors.transparent),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+                side: BorderSide(
+                  color: AppColors.yellow,
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+          onPressed: () => controller.signup(context),
+          child: Text(
+            'Cadastrar-se',
+            style: TextStyle(
+              fontSize: 24,
+              color: AppColors.yellow,
+            ),
+          ),
+        ),
+      ),
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -38,155 +144,34 @@ class _SignUpPageState extends State<SignUpPage> {
           ),
         ),
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            // O SingleChildScrollView FOI REMOVIDO DAQUI
-            child: Center(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  // Ensure the column takes at least the full available height
-                  minHeight: MediaQuery.of(context).size.height - 40,
-                ),
-                child: IntrinsicHeight(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Center(
+            child: isWeb
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(
                         'lib/app/assets/PolIG.png',
-                        width: MediaQuery.of(context).size.width * 0.6,
+                        width: 180,
                       ),
-
-                      Column(
-                        children: [
-                          // Grupo dos 4 campos de texto
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: CustomTextField(
-                              focusBorder: AppColors.yellow,
-                              controller: controller.nameController,
-                              labelText: 'Nome',
-                              onChanged: (value) => controller.setName(value),
-                              suffixIcon: Icon(
-                                Icons.person,
-                                color: AppColors.white,
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: CustomTextField(
-                              focusBorder: AppColors.yellow,
-                              suffixIcon: Icon(
-                                Icons.mail,
-                                color: AppColors.white,
-                              ),
-                              labelText: 'E-mail',
-                              onChanged: (value) => controller.setEmail(value),
-                              keyboardType: TextInputType.emailAddress,
-                              controller: controller.emailController,
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: CustomTextField(
-                              focusBorder: AppColors.yellow,
-                              controller: controller.passwordController,
-                              labelText: 'Senha',
-                              onChanged: (value) =>
-                                  controller.setPassword(value),
-                              suffixIcon: controller.isPasswordVisible
-                                  ? Icon(
-                                      Icons.visibility,
-                                      color: AppColors.white,
-                                    )
-                                  : Icon(
-                                      Icons.visibility_off,
-                                      color: AppColors.white,
-                                    ),
-                              isPassword: !controller.isPasswordVisible,
-                              onSuffixIconTap: () {
-                                setState(() {
-                                  controller.togglePasswordVisibility();
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: CustomTextField(
-                              focusBorder: AppColors.yellow,
-                              controller: controller.confirmPasswordController,
-                              labelText: 'Confirmar Senha',
-                              suffixIcon: controller.isConfirmPasswordVisible
-                                  ? Icon(
-                                      Icons.visibility,
-                                      color: AppColors.white,
-                                    )
-                                  : Icon(
-                                      Icons.visibility_off,
-                                      color: AppColors.white,
-                                    ),
-                              isPassword: !controller.isConfirmPasswordVisible,
-                              onSuffixIconTap: () {
-                                setState(() {
-                                  controller.toggleConfirmPasswordVisibility();
-                                });
-                              },
-                            ),
-                          ),
-
-                          SizedBox(height: 24), // Espaço
-                        ],
-                      ),
-
-                      // botão de "Cadastrar-se"
-                      Hero(
-                        tag: 'signupBtn',
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.8,
-                          height: MediaQuery.of(context).size.height * 0.062,
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              elevation: WidgetStateProperty.all(2),
-                              backgroundColor: WidgetStateProperty.all(
-                                Colors.transparent,
-                              ),
-                              shape: WidgetStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(6),
-                                  side: BorderSide(
-                                    color: AppColors.yellow,
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            onPressed: () => {
-                              // Navigator.pushNamedAndRemoveUntil(
-                              //   context,
-                              //   "/home",
-                              //   (route) => false,
-                              // ),
-                              controller.signup(context),
-                            },
-                            child: Text(
-                              'Cadastrar-se',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: AppColors.yellow,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      SizedBox(height: 30),
+                      fields,
+                      signupButton,
                     ],
+                  )
+                : Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          'lib/app/assets/PolIG.png',
+                          width: size.width * 0.6,
+                        ),
+                        fields,
+                        signupButton,
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ),
           ),
         ),
       ),
