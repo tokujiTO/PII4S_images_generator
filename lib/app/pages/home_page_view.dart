257aeb59855947c915a7b121394b4e6e3c0f27a1
Giovanna Lara <ghlar4sr@gmail.com>
2025-11-23 03:27:54 -0300

    feat: fix details of the pages in general

--- lib/app/pages/home_page_view.dart
+++ lib/app/pages/home_page_view.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:poliedroimagesgenerator/app/components/filter_button.dart';
 import 'package:poliedroimagesgenerator/app/components/text_field_ai.dart';
